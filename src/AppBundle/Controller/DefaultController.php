123fa31f402063eb88fa55ff31969e1a70eb7658
sebeweb <sebbeweb@gmail.fr>
2017-02-16 17:14:40 +0100

    gestion des utilisateur

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,15 +5,44 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         // replace this example code with whatever you need
-        return $this->redirect("/blue");
+        return $this->render("base.html.twig");
     }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function adminAction(Request $request) {
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout() {
+        return new Response('<html><body>logout page!</body></html>');
+    }
+
+    /**
+     * @Route("/login_check", name="logincheck")
+     */
+    public function loginCheck() {
+        return new Response('<html><body>logincheck page!</body></html>');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login() {
+//        return $this->render("default/login/login.html.twig");
+    }
+
 }
